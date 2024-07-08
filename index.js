ec96b2e5ef9ea261f3648632ad53f9c78a215ea6
varshney-dv <dv373737@gmail.com>
2024-07-09 01:04:19 +0530

    7th commit

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const modal=document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+const card=document.querySelector(".card");
 const openmodal=()=>{
     console.log("modal is open ");
     modal.classList.add("active");
     overlay.classList.add("overlayactive");
+    card.classList.add("blur")
 }
 const closemodal=()=>{
     console.log("modal is close ");
     modal.classList.remove("active");
     overlay.classList.remove("overlayactive");
+    card.classList.remove("blur");
 }
